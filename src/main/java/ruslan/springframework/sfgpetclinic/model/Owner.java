5a9853b5cfcd4cc95d9bfd508f96ddd66b19ea31
ruslansimeonov <ruslansimenov@gmail.com>
2021-09-30 14:29:53 +0200

    Adding POJOS.

---
+++ src/main/java/ruslan/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package ruslan.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/ruslan/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package ruslan.springframework.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
