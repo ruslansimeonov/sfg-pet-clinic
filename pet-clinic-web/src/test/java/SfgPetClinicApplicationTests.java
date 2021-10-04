072e63a5c038e276a1215263b1781cac71ab995b
ruslansimeonov <ruslansimenov@gmail.com>
2021-10-04 13:26:41 +0200

    Revert "Changing pet-clinic modules"
    This reverts commit dfbc1a2586537a2d61b074ce4c2aea1c2e6518fd.

--- pet-clinic-web/src/test/java/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/SfgPetClinicApplicationTests.java
@@ -1,11 +1,11 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class SfgPetClinicApplicationTests {
+class SfgPetClinicApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
